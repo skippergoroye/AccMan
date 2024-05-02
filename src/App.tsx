26d97402d2e37bc8012640ab207966bf30d5e56c
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-05-02 11:39:09 +0100

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  
+
+  return (
+   <div className="bg-red-500">
+     <Button>Click me</Button>
+   </div>
+  )
+}
+
+export default App
